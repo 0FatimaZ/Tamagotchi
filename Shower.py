2612729639fdae07307314a44935a5390d72be19
AmaliaEG <Amolahamudi@gmail.com>
2023-04-17 01:37:29 +0200

    Shower fikset

--- Shower.py
+++ Shower.py
@@ -14,60 +14,59 @@ PATH = "./Icons/"
 @client.event
 async def shower(client, message):
 
-    while True:
-        user = str(message.author.id)
-        (fridge, state) = Maintenance.users[user]
+    user = str(message.author.id)
+    (fridge, state) = Maintenance.users[user]
 
+    
+    reply = "Would you like to give Cato a shower?" + "if yes react with" + ': ' + ':shower:'
+    shower_message = await message.channel.send(reply)
+    await shower_message.add_reaction('🚿')  
+
+    def check(reaction, user):
+
+        return user == message.author and str(reaction.emoji) in ['🚿']
+    try:
+        reaction, user = await client.wait_for('reaction_add', timeout=10.0, check=check)
         
-        reply = "Would you like to give Cato a shower?" + "if yes react with" + ': ' + ':shower:'
-        shower_message = await message.channel.send(reply)
-        await shower_message.add_reaction('🚿')  
-
-        def check(reaction, user):
-
-            return user == message.author and str(reaction.emoji) in ['🚿']
+    except asyncio.TimeoutError:
         try:
-            reaction, user = await client.wait_for('reaction_add', timeout=10.0, check=check)
-            
-        except asyncio.TimeoutError:
-            try:
-                await shower_message.delete()
-            except discord.errors.NotFound:
-                pass
+            await shower_message.delete()
+        except discord.errors.NotFound:
+            pass
+    
+    else:
         
-        else:
+        if str(reaction.emoji) == '🚿':
+            await shower_message.delete()
+            if state["stats"].clean >= 2:
+                await message.channel.send("Your pet is already clean")
             
-            if str(reaction.emoji) == '🚿':
-                await shower_message.delete()
-                if state["stats"].clean >= 2:
-                    await message.channel.send("Your pet is already clean")
-                
-                elif state["stats"].clean < 2:
-                    reply = "Let's clean your pet" + '⏳'
-                    await message.channel.send(reply)
-                    await message.channel.send(file=discord.File(PATH + "Cleancat.png"))
-                    await asyncio.sleep(3) 
-                    reply = "Your pet is now clean" + '⌛' + "you recieved 1 buckaloue! :3"
-                    await message.channel.send(reply)
-                    state["buckaloues"] += 1
-                    print(state["buckaloues"])
-                    state["stats"].clean += 2
-                    state["stats"].happy -= 1
-                    if state["stats"].happy < 0:
-                        state["stats"].happy = 0
-                    print(str(state["stats"].clean))
-                
-                elif state["stats"].clean == 0:
-                    reply = "Your pet could really use a shower..." + '⏳'
-                    await message.channel.send(reply)
-                    await message.channel.send(file=discord.File(PATH + "Cleancat.png"))
-                    await asyncio.sleep(3) 
-                    reply = "Your pet is now clean" + '⌛' + "you recieved 1 buckaloue! :3"
-                    await message.channel.send(reply)
-                    state["buckaloues"] += 1
-                    state["stats"].clean += 2
-                
-                await Main.menu(message)
+            elif state["stats"].clean < 2:
+                reply = "Let's clean your pet" + '⏳'
+                await message.channel.send(reply)
+                await message.channel.send(file=discord.File(PATH + "Cleancat.png"))
+                await asyncio.sleep(3) 
+                reply = "Your pet is now clean" + '⌛' + "you recieved 1 buckaloue! :3"
+                await message.channel.send(reply)
+                state["buckaloues"] += 1
+                print(state["buckaloues"])
+                state["stats"].clean += 2
+                state["stats"].happy -= 1
+                if state["stats"].happy < 0:
+                    state["stats"].happy = 0
+                print(str(state["stats"].clean))
+            
+            elif state["stats"].clean == 0:
+                reply = "Your pet could really use a shower..." + '⏳'
+                await message.channel.send(reply)
+                await message.channel.send(file=discord.File(PATH + "Cleancat.png"))
+                await asyncio.sleep(3) 
+                reply = "Your pet is now clean" + '⌛' + "you recieved 1 buckaloue! :3"
+                await message.channel.send(reply)
+                state["buckaloues"] += 1
+                state["stats"].clean += 2
+            
+            await Main.menu(message)
                 
                 
 
