4b11f4dcb638483ed27e2fb456f4eca1010ce71c
0FatimaZ <fatima.alasmi2004@gmail.com>
2023-04-30 16:14:29 +0200

    pickle load i start

--- Start.py
+++ Start.py
@@ -10,7 +10,10 @@ client = discord.Client(intents=intents)
 @client.event
 async def start(message):
     user = str(message.author.id)
+    
     if user in Maintenance.users:
+        with open('StateDict.p', 'wb') as fp:
+            pickle.load(Maintenance.users, fp, protocol=pickle.HIGHEST_PROTOCOL)
         await message.channel.send("Welcome!")
     else:
         Maintenance.users.update({user: Maintenance.new_stats()})
